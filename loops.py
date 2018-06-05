60512800ca6b13ca511dd81a668a155a8312ee1c
lewie14 <lewie@netvigator.com>
2018-06-05 13:50:53 +0800

    First commit - Loops

---
+++ loops.py
@@ -0,0 +1,24 @@
+dog_breeds = ['french_bulldog', 'dalmation', 'shihtzu', 'poodle', 'collie']
+
+for breed in dog_breeds:
+    print(breed)
+   
+#----------------------------------------
+board_games = ['Settlers of Catan', 'Carcassone', 'Power Grid', 'Agricola', 'Scrabble']
+
+sport_games = ['football', 'football - American', 'hockey', 'baseball', 'cricket']
+
+for game in board_games:
+	print(game)
+  
+for sport in sport_games:
+	print(sport)
+	
+#-----------------------------------------------
+
+promise = "I will not chew gum in class"
+
+for i in range(5):
+  print(promise)
+#-------------------------------------------
+
